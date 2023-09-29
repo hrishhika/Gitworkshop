62f7750a86f2abe624406218108875ba21c62c66
hrishhika <107240209+hrishhika@users.noreply.github.com>
2023-09-29 10:55:01 +0545

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,22 @@
+<!-- footer.php -->
+
+<footer>
+    <div class="container">
+        <div class="footer-content">
+            <div class="footer-section about">
+                <h3>About Us</h3>
+                <p>Your Online Food Delivery System is committed to providing quality food and excellent service to our customers. Order now and experience the best in culinary delight!</p>
+            </div>
+            <div class="footer-section contact">
+                <h3>Contact Us</h3>
+                <p>
+                    <span>Email: foodOrder@email.com</span><br>
+                    <span>Phone: +977 9888888888</span>
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <p>&copy; <?php echo date('Y'); ?> Your Online Food Delivery System. All Rights Reserved.</p>
+    </div>
+</footer>
